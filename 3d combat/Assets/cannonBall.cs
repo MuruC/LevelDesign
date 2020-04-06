56a329e2d1b9fec7256cee9ecdb11b1316bf2b3b
Muru_C <mc6251@nyu.edu>
2020-04-06 20:03:21 +0800

    cannon

---
+++ 3d combat/Assets/cannonBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonBall : MonoBehaviour
+{
+    // Use this for initialization
+    public float speed = 0.0f;
+    void Start()
+    {
+        Destroy(gameObject, 0.70f);                      //消失
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(speed, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(transform.right * Time.deltaTime * 2);    //移动    
+
+    }
+}
---
+++ 3d combat/Assets/projectCannonBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectCannonBall : MonoBehaviour
+{
+    public GameObject ball;
+    public float t1;
+    private float t2 = 1.0f;
+    bool bDestroy = false;
+    public float speed;
+    void Start()
+    {
+
+        InvokeRepeating("CloneSphere", t1, t2); //调用更简洁的实例化方法：重复运行一个方法，起始时间：几秒后开始行； 几秒发射一个
+
+    }
+
+    void Update()
+    {
+        if (bDestroy == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void CloneSphere()
+    {
+        Instantiate(ball);
+        ball.GetComponent<cannonBall>().speed = speed;
+        ball.transform.position = gameObject.transform.position;
+    }
+
+    public void setDestroy()
+    {
+        bDestroy = true;
+    }
+}
